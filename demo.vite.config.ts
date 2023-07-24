e99f59f417a835de3c75e6350d1afbd0c85631a3
zlenner <119705166+zlenner@users.noreply.github.com>
2023-07-24 16:40:03 +0300

    docs-deploy

---
+++ demo.vite.config.ts
@@ -0,0 +1,12 @@
+// vite.config.ts
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "/zlenner/device-auth",
+  build: {
+    outDir: 'demo-dist',
+  },
+})
