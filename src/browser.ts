edc0c5ac0264b384c1e7caec76ed121e1ce9de6c
zlenner <119705166+zlenner@users.noreply.github.com>
2023-07-25 02:34:12 +0300

    puk

--- src/browser.ts
+++ src/browser.ts
@@ -23,7 +23,6 @@ export const signIn = async (challenge: string): Promise<EitherPayload> => {
     } else {
         // Challenge can be empty, registration does not require a challenge.
         // But since we've loaded one, why not use it?
-        console.log(generate(4))
         const username = generate(4)
         const registration = await client.register(username, challenge, {
             "authenticatorType": "auto",
